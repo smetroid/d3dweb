f229dc5ed73063e329690a3826a41aa9b32adf92
EnriqueC <enriquec@vivaldi.net>
2024-03-03 18:05:54 -0700

    adding mdi icons

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi'
+  },
   components,
   directives,
   ssr: true,
