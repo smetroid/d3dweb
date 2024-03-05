dd69e82aeb8fc7b76aa9e45b6f48729d524405ff
EnriqueC <enriquec@vivaldi.net>
2024-03-05 12:02:08 -0700

    updating settings dialog, removing a lot of unnecessary code

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,75 +1,78 @@
 <template>
   <div>
-    <v-dialog ref="settings" v-model="settingsModal" max-width="500" @keydown.esc="common($event)">
-      <focus-trap v-model="enableTrap"
-        tabindex="-1"
+    <v-dialog
+      ref="settings"
+      v-model="settingsModal"
+      max-width="500"
+      @keydown.esc="common($event)"
+      >
+      <focus-trap v-model:active="enableTrap"
         class="trap is-active">
-        <div>
-          <v-card class="mx-auto" max-width="760">
+        <div tabindex="0">
+          <v-card
+            class="text-primary"
+            >
               <v-card-title
-                class="headline">
-                <v-row justify="center">
+                class="bg-primary d-flex justify-center">
                   <b>Settings</b>
-                </v-row>
               </v-card-title>
-              <v-card-text class="blue-grey darken-4">
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">Debug:</span>
-                  </v-col>
-                  <v-switch v-model="settings.debug">
-                  </v-switch>
-                </v-row>
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">Reset:</span>
-                  </v-col>
-                  <v-switch v-model="settings.reset">
-                  </v-switch>
-                </v-row>
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">Hints:</span>
-                  </v-col>
-                  <v-text-field dense outline v-model="settings.hints"
-                    placeholder="asdfjklqweruiopzxcvnmgh"
-                    @keypress.stop.prevent="">
-                  </v-text-field>
-                </v-row>
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">Hint Link Color:</span>
-                  </v-col>
-                  <v-text-field dense outline v-model="settings.hintLinkColor"
-                    placeholder=""
-                    @keypress.stop.prevent="">
-                  </v-text-field>
-                </v-row>
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">Hint Background Color:</span>
-                  </v-col>
-                  <v-text-field dense outline v-model="settings.hintBGColor"
-                    placeholder=""
-                    @keypress.stop.prevent="">
-                  </v-text-field>
-                </v-row>
-                <v-row align="center">
-                  <v-col sm="3">
-                    <span class="samus__label">D3 Edge Line:</span>
-                  </v-col>
-                  <v-radio-group row dense v-model="settings.d3Line">
-                    <v-radio dense v-for="n in d3EdgeLine"
-                      :key="n.value"
-                      :label="`${n.label}`"
-                      :value="n.value"></v-radio>
-                  </v-radio-group>
-                </v-row>
+              <v-card-text
+                color="primary"
+                class="">
+                <v-switch
+                  color="primary"
+                  v-model="settings.debug"
+                  label="Debug"
+                >
+                </v-switch>
+                <v-switch
+                  color="primary"
+                  v-model="settings.showHelp"
+                  label="Help Pane"
+                >
+                </v-switch>
+                <v-switch 
+                  color="primary"
+                  v-model="settings.reset"
+                  label="Reset"
+                >
+                </v-switch>
+                <v-text-field
+                  v-model="settings.hints"
+                  label="Hints"
+                  placeholder="asdfjklqweruiopzxcvnmgh"
+                  @keypress.stop.prevent="">
+                </v-text-field>
+                <v-text-field 
+                  v-model="settings.hintLinkColor"
+                  label="Hint Link Color"
+                  placeholder=""
+                  @keypress.stop.prevent="">
+                </v-text-field>
+                <v-text-field 
+                  label="Hint Background Color"
+                  v-model="settings.hintBGColor"
+                  placeholder=""
+                  @keypress.stop.prevent="">
+                </v-text-field>
+                <v-radio-group 
+                  label="D3 Edge Line"
+                  color="primary"
+                  v-model="settings.d3Line">
+                  <v-radio dense v-for="n in d3EdgeLine"
+                    :key="n.value"
+                    :label="`${n.label}`"
+                    :value="n.value"></v-radio>
+                </v-radio-group>
               </v-card-text>
-              <v-card-actions>
-                <v-btn dense color="green" @click="save()">Save</v-btn>
+              <v-card-actions 
+                class="bg-primary">
+                <v-btn 
+                  class="bg-green"
+                  @click="save()">Save</v-btn>
                 <v-spacer></v-spacer>
-                <v-btn dense class="ma-2" outlined color="red"
+                <v-btn 
+                  class="bg-red"
                   @click="close()">Close</v-btn>
               </v-card-actions>
           </v-card>
@@ -84,12 +87,12 @@ import VueCookies from 'vue-cookies'
 // Vue.use(VueCookies)
 // import VueCookie from '@/'
 export default {
-  name: 'Settings',
+  name: 'SettingsDialog',
   props: ['active'],
   data () {
     return {
       options: [],
-      gNavLi: null,
+      showHelp: false,
       settingsModal: false,
       enableTrap: false,
       theme: null,
@@ -187,9 +190,5 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.samus__label {
-  color: #42b983;
-  font-weight: 1000;
-}
 
 </style>
